fc5147376ade99e877c6f4e920af662962c2b9db
asterka <askar.master@yandex.ru>
2021-03-23 02:39:32 +0300

    Adding my first DAG

---
+++ dags/my_first_dag.py
@@ -0,0 +1,33 @@
+try:
+
+    from datetime import timedelta
+    from airflow import DAG
+    from airflow.operators.python_operator import PythonOperator
+    from datetime import datetime
+    import pandas as pd
+
+    print("All Dag modules are ok ......")
+except Exception as e:
+    print("Error  {} ".format(e))
+
+
+def first_function_execute():
+    print("Hello World!")
+
+with DAG(
+    dag_id="my_first_dag.py",
+    schedule_interval="@hourly",
+    default_args={
+        "owner":"airflow",
+        "retries":1,
+        "retry_delay":timedelta(minutes=10),
+        "start_date":datetime(2021,23,3)
+    },
+    cathcup=False
+) as f:
+    first_function_execute = PythonOperator(
+        task_id="first_function_execute",
+        python_callable=first_function_execute)
+
+
+first_function_execute >> second_function_execute
