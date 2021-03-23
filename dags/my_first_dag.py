b1f76acebf8da9ac6dcf926d2b03b6c6dd9dfd21
asterka <askar.master@yandex.ru>
2021-03-23 04:13:08 +0300

    modified:   dags/my_first_dag.py

--- dags/my_first_dag.py
+++ dags/my_first_dag.py
@@ -1,76 +1,17 @@
-from datetime import timedelta
+import datetime as dt
 
-import airflow
 from airflow import DAG
 from airflow.operators.bash_operator import BashOperator
+from airflow.operators.python_operator import PythonOperator
 
-# These args will get passed on to each operator
-# You can override them on a per-task basis during operator initialization
-default_args = {
-    'owner': 'airflow',
-    'depends_on_past': False,
-    'start_date': airflow.utils.dates.days_ago(2),
-    'email': ['airflow@example.com'],
-    'email_on_failure': False,
-    'email_on_retry': False,
-    'retries': 1,
-    'retry_delay': timedelta(minutes=5),
-    # 'queue': 'bash_queue',
-    # 'pool': 'backfill',
-    # 'priority_weight': 10,
-    # 'end_date': datetime(2016, 1, 1),
-    # 'wait_for_downstream': False,
-    # 'dag': dag,
-    # 'adhoc':False,
-    # 'sla': timedelta(hours=2),
-    # 'execution_timeout': timedelta(seconds=300),
-    # 'on_failure_callback': some_function,
-    # 'on_success_callback': some_other_function,
-    # 'on_retry_callback': another_function,
-    # 'trigger_rule': u'all_success'
-}
 
-dag = DAG(
-    'tutorial',
-    default_args=default_args,
-    description='A simple tutorial DAG',
-    schedule_interval=timedelta(days=1),
-)
+def greet():
+    print('Trying to write to the file')
+    with open('./greet.txt', 'a+', encoding='utf8') as f:
+        now = dt.datetime.now()
+        t = now.strftime("%Y-%m-%d %H:%M")
+        f.write(str(t) + '\n')
+    return 'Greeted'
 
-# t1, t2 and t3 are examples of tasks created by instantiating operators
-t1 = BashOperator(
-    task_id='print_date',
-    bash_command='date',
-    dag=dag,
-)
-
-t1.doc_md = """\
-Hello
-"""
-
-dag.doc_md = __doc__
-
-t2 = BashOperator(
-    task_id='sleep',
-    depends_on_past=False,
-    bash_command='sleep 5',
-    dag=dag,
-)
-
-templated_command = """
-{% for i in range(5) %}
-    echo "{{ ds }}"
-    echo "{{ macros.ds_add(ds, 7)}}"
-    echo "{{ params.my_param }}"
-{% endfor %}
-"""
-
-t3 = BashOperator(
-    task_id='templated',
-    depends_on_past=False,
-    bash_command=templated_command,
-    params={'my_param': 'My parameter'},
-    dag=dag,
-)
-
-t1 >> [t2, t3]
+def respond():
+    return 'Greet Responded Again'
