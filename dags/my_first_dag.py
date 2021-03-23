7c21bc81be12b7df20d51132a19a1950cbc912d9
asterka <askar.master@yandex.ru>
2021-03-23 03:36:47 +0300

    modified:   dags/my_first_dag.py 	modified:   docker-compose.yml

--- dags/my_first_dag.py
+++ dags/my_first_dag.py
@@ -1,54 +1,76 @@
-try:
+from datetime import timedelta
 
-    from datetime import timedelta
-    from airflow import DAG
-    from airflow.operators.python_operator import PythonOperator
-    from datetime import datetime
-    import pandas as pd
+import airflow
+from airflow import DAG
+from airflow.operators.bash_operator import BashOperator
 
-    print("All Dag modules are ok ......")
-except Exception as e:
-    print("Error  {} ".format(e))
+# These args will get passed on to each operator
+# You can override them on a per-task basis during operator initialization
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'start_date': airflow.utils.dates.days_ago(2),
+    'email': ['airflow@example.com'],
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5),
+    # 'queue': 'bash_queue',
+    # 'pool': 'backfill',
+    # 'priority_weight': 10,
+    # 'end_date': datetime(2016, 1, 1),
+    # 'wait_for_downstream': False,
+    # 'dag': dag,
+    # 'adhoc':False,
+    # 'sla': timedelta(hours=2),
+    # 'execution_timeout': timedelta(seconds=300),
+    # 'on_failure_callback': some_function,
+    # 'on_success_callback': some_other_function,
+    # 'on_retry_callback': another_function,
+    # 'trigger_rule': u'all_success'
+}
 
+dag = DAG(
+    'tutorial',
+    default_args=default_args,
+    description='A simple tutorial DAG',
+    schedule_interval=timedelta(days=1),
+)
 
-def first_function_execute(**context):
-    print("first_function_execute   ")
-    context['ti'].xcom_push(key='mykey', value="first_function_execute says Hello ")
+# t1, t2 and t3 are examples of tasks created by instantiating operators
+t1 = BashOperator(
+    task_id='print_date',
+    bash_command='date',
+    dag=dag,
+)
 
+t1.doc_md = """\
+Hello
+"""
 
-def second_function_execute(**context):
-    instance = context.get("ti").xcom_pull(key="mykey")
-    data = [{"name":"Soumil","title":"Full Stack Software Engineer"}, { "name":"Nitin","title":"Full Stack Software Engineer"},]
-    df = pd.DataFrame(data=data)
-    print('@'*66)
-    print(df.head())
-    print('@'*66)
+dag.doc_md = __doc__
 
-    print("I am in second_function_execute got value :{} from Function 1  ".format(instance))
+t2 = BashOperator(
+    task_id='sleep',
+    depends_on_past=False,
+    bash_command='sleep 5',
+    dag=dag,
+)
 
+templated_command = """
+{% for i in range(5) %}
+    echo "{{ ds }}"
+    echo "{{ macros.ds_add(ds, 7)}}"
+    echo "{{ params.my_param }}"
+{% endfor %}
+"""
 
-with DAG(
-        dag_id="first_dag",
-        schedule_interval="@daily",
-        default_args={
-            "owner": "airflow",
-            "retries": 1,
-            "retry_delay": timedelta(minutes=5),
-            "start_date": datetime(2021, 1, 1),
-        },
-        catchup=False) as f:
+t3 = BashOperator(
+    task_id='templated',
+    depends_on_past=False,
+    bash_command=templated_command,
+    params={'my_param': 'My parameter'},
+    dag=dag,
+)
 
-    first_function_execute = PythonOperator(
-        task_id="first_function_execute",
-        python_callable=first_function_execute,
-        provide_context=True,
-        op_kwargs={"name":"Soumil Shah"}
-    )
-
-    second_function_execute = PythonOperator(
-        task_id="second_function_execute",
-        python_callable=second_function_execute,
-        provide_context=True,
-    )
-
-first_function_execute >> second_function_execute
+t1 >> [t2, t3]
